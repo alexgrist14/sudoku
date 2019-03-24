50a487bc0ab741b1e33b5479a67b418478eb7195
alexgrist14 <43311575+alexgrist14@users.noreply.github.com>
2019-03-24 15:48:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ module.exports = function solveSudoku(matrix) {
   // [9, 6, 1, 5, 3, 7, 2, 8, 4],
   // [2, 8, 7, 4, 1, 9, 6, 3, 5],
   // [3, 4, 5, 2, 8, 6, 1, 7, 9]
-//https://en.wikipedia.org/wiki/Sudoku_solving_algorithms
 
 SolveSudoku(matrix);
 return matrix;
